a9277f8c5168c7afae28edea461fb24cfe97f364
catse2000 <catse2000@yahoo.com>
2020-10-07 21:05:38 -0500

    redo code as original wasn't saved

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const PORT = process.env.PORT || 3001;
 const app = express();
 
 // parse incoming string or array data
-app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({ extended: true}));
 
 // parse incoming JSON data
 app.use(express.json());
@@ -60,12 +60,12 @@ function createNewAnimal(body, animalsArray) {
     const animal = body;
     animalsArray.push(animal);
     fs.writeFileSync(
-        path.join(__dirname, './data/animals.json'),
+        path.join(__dirname, './data/animals/json'),
         JSON.stringify({ animals: animalsArray }, null, 2)
     );
 
     return animal;
-}
+};
 
 app.get('/api/animals', (req, res) => {
     let results = animals;
@@ -86,17 +86,16 @@ app.get('/api/animals/:id', (req, res) => {
 });
 
 app.post('/api/animals', (req, res) => {
-    //set id based on what the next index of the array will be
+    // set id based onw hat the next index of the array will be
     req.body.id = animals.length.toString();
-
+    
     // if any data in req.body is incorrect, send 400 error back
     if (!validateAnimal(req.body)) {
-        res.status(400).send('The animal is not properly formatted!');
+        res.status(400).send('The animal is not properly formatted.');
     }
-    else{
-        // add animal to json file and animals array in this function
+    else {
         const animal = createNewAnimal(req.body, animals);
-
+        
         // req.body is where our incoming content will be
         res.json(req.body);
     }
@@ -106,17 +105,17 @@ app.listen(PORT, () => {
     console.log(`API server now on port ${PORT}!`);
 });
 
-function validateAnimal(animal) {
+function validateAnimal(animal){
     if (!animal.name || typeof animal.name !== 'string') {
         return false;
     }
-    if(!animal.species || typeof animal.species !== 'string') {
+    if (!animal.species || typeof animal.species !== 'string') {
         return false;
     }
-    if(!animal.diet || typeof animal.diet !== 'string') {
+    if (!animal.diet || typeof animal.diet !== 'string') {
         return false;
     }
-    if (!animal.personalityTraits || !Array.isArray(animal.personalityTraits)){
+    if (!animal.personalityTraits || !Array.isArray(animal.personalityTraits)) {
         return false;
     }
     return true;
